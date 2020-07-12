3524c6aa4a8dd36379d9ce45f4c967c8fa8056c8
Rafael Correa <rafael.correa@sap.com>
2020-07-12 17:14:30 -0300

    Initial commit

---
+++ src/test/kotlin/rpg_combat/mainTest.kt
@@ -0,0 +1,10 @@
+package rpg_combat
+
+import kotlin.test.assertEquals
+import org.junit.Test
+
+class MainTest {
+    @Test fun f() {
+        assertEquals("Hello, world!", getGreeting())
+    }
+}
